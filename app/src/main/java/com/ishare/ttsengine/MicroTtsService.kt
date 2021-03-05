ab50e03961e8da0509b054e8c321d9bd7c89e7bf
bingoHua <bingo0206@foxmail.com>
2021-03-06 05:17:14 +0800

    优化速度

--- app/src/main/java/com/ishare/ttsengine/MicroTtsService.kt
+++ app/src/main/java/com/ishare/ttsengine/MicroTtsService.kt
@@ -1,8 +1,9 @@
 package com.ishare.ttsengine
 
 import android.media.AudioFormat
-import android.media.AudioFormat.ENCODING_PCM_16BIT
 import android.media.MediaPlayer
+import android.os.Handler
+import android.os.Looper
 import android.speech.tts.SynthesisCallback
 import android.speech.tts.SynthesisRequest
 import android.speech.tts.TextToSpeech
@@ -15,29 +16,27 @@ import com.microsoft.cognitiveservices.speech.audio.AudioConfig
 import freemarker.template.Configuration
 import freemarker.template.TemplateExceptionHandler
 import org.apache.commons.codec.digest.DigestUtils
+import java.io.File
 import java.io.FileInputStream
 import java.io.StringWriter
 import java.util.*
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.CyclicBarrier
 import java.util.logging.Logger
 import kotlin.collections.HashMap
 
 
 private const val SAMPLING_RATE_HZ = 16000
 
-class MicroTtsService : TextToSpeechService(), MediaPlayer.OnPreparedListener,
-        MediaPlayer.OnErrorListener,
-        MediaPlayer.OnCompletionListener {
-    lateinit var speechConfig: SpeechConfig
-    lateinit var audioConfig: AudioConfig
-    lateinit var synthesizer: SpeechSynthesizer
+class MicroTtsService : TextToSpeechService() {
 
     lateinit var mCurrentLanguage: Array<String>
-    lateinit var cfg: Configuration
     lateinit var app: MoxiangApplication
-    lateinit var fileCachePath: String
     lateinit var callback: SynthesisCallback
-    private val player by lazy { MediaPlayer() }
-    val logger = Logger.getLogger("TAG")
+    lateinit var audioDownloader: AudioDownloader
+    lateinit var speechManager: SpeechManager
+    private val logger: Logger = Logger.getLogger("TAG")
+    lateinit var handler: Handler
 
     @Volatile
     private var mStopRequested = false
@@ -55,22 +54,54 @@ class MicroTtsService : TextToSpeechService(), MediaPlayer.OnPreparedListener,
 
     override fun onDestroy() {
         super.onDestroy()
-        player.release()
+        mStopRequested = true
+        speechManager.stopSpeech()
+        audioDownloader.stopBlockingDownloadTask()
     }
 
-    fun initEngine() {
-        speechConfig = SpeechConfig.fromSubscription("9644ad9e4a40402a83462228bfeca076", "eastus")
-        cfg = Configuration(Configuration.VERSION_2_3_24)
-        cfg.setDirectoryForTemplateLoading(app.offlineDir)
-        cfg.defaultEncoding = "UTF-8"
-        cfg.templateExceptionHandler = TemplateExceptionHandler.DEBUG_HANDLER;
-        player.setOnErrorListener(this)
-        player.setOnPreparedListener(this)
-        player.setOnCompletionListener(this)
+    private fun initEngine() {
+        audioDownloader = AudioDownloader(app)
+        audioDownloader.setCallback(object : AudioDownloader.DownloadCallback {
+            override fun complete(fileUrl: String, requestData: RequestData) {
+                speechManager.addSpeechTask(AudioData(fileUrl, requestData))
+            }
+        })
+        audioDownloader.starBlockingDownloadTask()
+        speechManager = SpeechManager()
+        speechManager.setCallback(object : SpeechManager.SpeechCallback {
+            override fun singleSpeechComplete(requestData: RequestData) {
+                /*callback.start(SAMPLING_RATE_HZ, AudioFormat.ENCODING_PCM_16BIT, 1)
+                callback.rangeStart(0, length, 0)*/
+                handler.post {
+                    requestData.callback.rangeStart(0, requestData.request.charSequenceText.length, 0)
+                }
+            }
+
+            override fun startSpeech(requestData: RequestData) {
+                /*callback.start(SAMPLING_RATE_HZ, AudioFormat.ENCODING_PCM_16BIT, 1)
+                callback.rangeStart(0, length, 0)
+*/
+                /*     callback.start(
+                             0,
+                             AudioFormat.ENCODING_PCM_16BIT, 1)*/
+                handler.post {
+                    requestData.callback.start(0,
+                            AudioFormat.ENCODING_PCM_16BIT, 1)
+                }
+
+            }
+
+            override fun speechError(mp: MediaPlayer, what: Int, extra: Int): Boolean {
+                callback.error()
+                //todo,回调有点问题
+                return true
+            }
+
+        })
+        speechManager.startSpeech()
     }
 
     override fun onIsLanguageAvailable(lang: String?, country: String?, variant: String?): Int {
-
 
 //        Log.i(TAG, "onIsLanguageAvailable lang=" + lang + " country=" + country + " variant=" + variant);
         return if (Locale.SIMPLIFIED_CHINESE.isO3Language == lang || Locale.US.isO3Language == lang) {
@@ -93,17 +124,20 @@ class MicroTtsService : TextToSpeechService(), MediaPlayer.OnPreparedListener,
 
     override fun onStop() {
         mStopRequested = true
-        player.stop()
-        player.release()
+        speechManager.stopSpeech()
+        audioDownloader.stopBlockingDownloadTask()
     }
 
-    override fun onSynthesizeText(request: SynthesisRequest?, callback: SynthesisCallback) {
+    inner class RequestData(val request: SynthesisRequest, val callback: SynthesisCallback)
+
+    override fun onSynthesizeText(request: SynthesisRequest, callback: SynthesisCallback) {
         this.callback = callback
+        handler = Handler(Looper.myLooper())
         // Note that we call onLoadLanguage here since there is no guarantee
         // that there would have been a prior call to this function.
         // Note that we call onLoadLanguage here since there is no guarantee
         // that there would have been a prior call to this function.
-        val load = onLoadLanguage(request!!.language, request.country,
+        val load = onLoadLanguage(request.language, request.country,
                 request.variant)
         // We might get requests for a language we don't support - in which case
         // we error out early before wasting too much time.
@@ -123,51 +157,161 @@ class MicroTtsService : TextToSpeechService(), MediaPlayer.OnPreparedListener,
         // We denote that we are ready to start sending audio across to the
         // framework. We use a fixed sampling rate (16khz), and send data across
         // in 16bit PCM mono.
-        callback.start(SAMPLING_RATE_HZ,
-                AudioFormat.ENCODING_PCM_16BIT, 1 /* Number of channels. */)
+        /*callback.start(SAMPLING_RATE_HZ,
+                AudioFormat.ENCODING_PCM_16BIT, 1 *//* Number of channels. *//*)*/
         val text = request.charSequenceText.toString()
         logger.info("text=${text}")
         if (text.isEmpty()) {
-            callback.error()
+            this.callback.error()
             return
         }
-        fileCachePath = getExternalFilesDir(null).absolutePath + "/${text.md5()}.wav"
-        audioConfig = AudioConfig.fromWavFileOutput(fileCachePath)
-        synthesizer = SpeechSynthesizer(speechConfig, audioConfig)
-        val result = synthesizer.SpeakSsml(text.toSsml(cfg))
-        if (result.reason == ResultReason.SynthesizingAudioCompleted) {
-            val file = fileCachePath
-            try {
-                player.setDataSource(FileInputStream(file).fd)
-                player.prepareAsync()
-            } catch (e: Exception) {
-                e.printStackTrace()
-                callback.done()
+        if (!mStopRequested) {
+            audioDownloader.addTask(RequestData(request, callback))
+        }
+    }
+
+    class AudioDownloader constructor(val app: MoxiangApplication) {
+        private var speechConfig: SpeechConfig = SpeechConfig.fromSubscription("9644ad9e4a40402a83462228bfeca076", "eastus")
+        private var cfg: Configuration = Configuration(Configuration.VERSION_2_3_24)
+
+        private lateinit var audioConfig: AudioConfig
+        private lateinit var synthesizer: SpeechSynthesizer
+        private val inQueue = ArrayBlockingQueue<RequestData>(100)
+        private var downloadCallback: DownloadCallback? = null
+        private var isStop = true
+        private val logger: Logger = Logger.getLogger("AudioDownloader")
+
+        init {
+            cfg.setDirectoryForTemplateLoading(app.offlineDir)
+            cfg.defaultEncoding = "UTF-8"
+            cfg.templateExceptionHandler = TemplateExceptionHandler.DEBUG_HANDLER;
+        }
+
+        fun addTask(request: RequestData) {
+            inQueue.put(request)
+        }
+
+        fun starBlockingDownloadTask() {
+            logger.info("starBlockingDownloadTask")
+            if (isStop) {
+                Thread(object : Runnable {
+                    override fun run() {
+                        while (!isStop) {
+                            val requestData = inQueue.take()
+                            if (!isStop) {
+                                logger.info("startDownload.text${requestData.request.charSequenceText}")
+                                download(requestData.request.charSequenceText.toString()).apply {
+                                    if (this.isNotEmpty()) {
+                                        logger.info("downloadComplete.text${requestData.request.charSequenceText}")
+                                        downloadCallback?.complete(this, requestData)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }).start()
+            } else {
+                throw RuntimeException("starBlockingDownloadTask already started!!")
+            }
+            isStop = false
+        }
+
+        fun stopBlockingDownloadTask() {
+            isStop = true
+        }
+
+        fun download(text: String): String {
+            val fileCachePath = app.getExternalFilesDir(null).absolutePath + "/${text.md5()}.wav"
+            return if (File(fileCachePath).exists()) {
+                fileCachePath
+            } else {
+                audioConfig = AudioConfig.fromWavFileOutput(fileCachePath)
+                synthesizer = SpeechSynthesizer(speechConfig, audioConfig)
+                val result = synthesizer.SpeakSsml(text.toSsml(cfg))
+                if (result.reason == ResultReason.SynthesizingAudioCompleted) fileCachePath else ""
             }
         }
+
+        fun setCallback(callback: DownloadCallback) {
+            this.downloadCallback = callback
+        }
+
+        interface DownloadCallback {
+            fun complete(fileUrl: String, requestData: RequestData)
+        }
+
     }
 
-    override fun onPrepared(mp: MediaPlayer?) {
-        if (!mStopRequested) {
-            logger.info("prepared.")
-            mp?.start()
-            callback.start(0, ENCODING_PCM_16BIT, 1)
+    data class AudioData(val audioUrl: String, val requestData: RequestData)
+
+    class SpeechManager : MediaPlayer.OnErrorListener,
+            MediaPlayer.OnCompletionListener {
+        private val speechQueue: ArrayBlockingQueue<AudioData> = ArrayBlockingQueue(100)
+        private var isStopped = true
+        private val player: MediaPlayer = MediaPlayer()
+        private val cyclicBarrier = CyclicBarrier(2)
+        private var callback: SpeechCallback? = null
+        private lateinit var lastAudioData: AudioData
+        private val logger: Logger = Logger.getLogger("SpeechManager")
+
+        init {
+            player.setOnErrorListener(this)
+            player.setOnCompletionListener(this)
+        }
+
+        fun addSpeechTask(audioData: AudioData) {
+            speechQueue.add(audioData)
+        }
+
+        fun startSpeech() {
+            //todo,可能会有线程问题
+            Thread {
+                while (!isStopped) {
+                    val audioData = speechQueue.take()
+                    logger.info("start speech.")
+                    player.setDataSource(FileInputStream(audioData.audioUrl).fd)
+                    lastAudioData = audioData
+                    player.prepare()
+                    if (!isStopped) {
+                        player.start()
+                        callback?.startSpeech(audioData.requestData)
+                        logger.info("start real speech.")
+                        cyclicBarrier.await()
+                    }
+                }
+            }.start()
+            isStopped = false
+        }
+
+        fun stopSpeech() {
+            isStopped = true
+            if (player.isPlaying) {
+                player.stop()
+            }
+            player.release()
+        }
+
+        interface SpeechCallback {
+            fun singleSpeechComplete(requestData: RequestData)
+            fun startSpeech(requestData: RequestData)
+            fun speechError(mp: MediaPlayer, what: Int, extra: Int): Boolean
+        }
+
+        override fun onError(mp: MediaPlayer, what: Int, extra: Int): Boolean {
+            return callback?.speechError(mp, what, extra) ?: true
+        }
+
+        override fun onCompletion(mp: MediaPlayer) {
+            player.reset()
+            cyclicBarrier.await()
+            logger.info("speech complete.")
+            callback?.singleSpeechComplete(lastAudioData.requestData)
+        }
+
+        fun setCallback(callback: SpeechCallback) {
+            this.callback = callback
         }
     }
-
-    override fun onError(mp: MediaPlayer?, what: Int, extra: Int): Boolean {
-        logger.info("onError.${what}")
-        player.reset()
-        callback.error()
-        return true
-    }
-
-    override fun onCompletion(mp: MediaPlayer?) {
-        logger.info("completion.")
-        player.reset()
-        callback.done()
-    }
-
 
 }
 
